5b9e1c82ba5970e7442e951345875a7da03ca662
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-06 10:34:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const laptopRoutes = require('./routes/laptops');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/laptops', laptopRoutes);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(Server running on port ${PORT}));
