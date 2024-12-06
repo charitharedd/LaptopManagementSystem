efcf68487aa68af0e72c2eaaa009927242d0ea8f
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-06 10:33:10 +0530

    Update laptop.js

--- models/laptop.js
+++ models/laptop.js
@@ -1,1 +1,10 @@
 
+const mongoose = require('mongoose');
+const LaptopSchema = new mongoose.Schema({
+    brand: String,
+    model: String,
+    serialNumber: String,
+    status: { type: String, enum: ['available', 'assigned', 'maintenance'], default: 'available' },
+    purchaseDate: Date,
+});
+module.exports = mongoose.model('Laptop', LaptopSchema);
